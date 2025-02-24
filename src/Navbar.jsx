7d241d3f22ed088bd5a171fde4402df1ce2d3e6d
kouski <72550619+kouski@users.noreply.github.com>
2025-02-24 21:54:50 +0100

    navbar responsive

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
-    const toogleLinks = ()=>{
-        setShowLinks(!showLinks)
-    }
+  const toogleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : "0px",
+  };
 
   return (
     <nav>
@@ -20,23 +28,28 @@ const Navbar = () => {
           </button>
         </div>
 
-        <div className={showLinks ? 'links-container show-container': 'links-container'}>
-        <ul className="links">
-          
-              {links.map((link) => {
-                return <li key={link.id}>
-                    <a href={link.url}>{link.text}</a>
-                    </li>;
-              })}
-            
-              {/* {social.map((icon) => {
-                return <li key={icon.id}>{icon.icon}</li>;
-              })} */}
-            
-        </ul>
+        <div
+          className="links-container"
+          ref={linksContainerRef}
+          style={linkStyles}
+        >
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              return (
+                <li key={link.id}>
+                  <a href={link.url}>{link.text}</a>
+                </li>
+              );
+            })}
+          </ul>
         </div>
-
-        
+        <ul className="social-icons">
+        {social.map((icon) => {
+          return <li key={icon.id}>
+            <a href={icon.url}>{icon.icon}</a>
+            </li>;
+        })}
+        </ul>
       </div>
     </nav>
   );
